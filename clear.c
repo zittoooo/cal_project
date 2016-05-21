782678b300404c0a49416b164e4fd2060905c669
jaeho93 <jaeho8032@gmail.com>
2016-05-21 21:30:39 +0900

    clear 수정

--- clear.c
+++ clear.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 int cl(int i){
 		system("clear");
 	return 0;
 }
 int main(void){
+	int i = 0;
 	char Input[60];
  	char clear[60] = "clear";
 	scanf("%s", Input);
-	if ( Input[60] == clear[60])
+	i = strcmp (Input, clear);
+	if (i == 0)
 		cl(1); 
 	return 0;
 }
